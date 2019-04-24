22eece2757e443f8225ecf736297689e7b908cdc
SauloFilho <saulosilvaf@gmail.com>
2019-04-24 00:09:55 -0300

    up
    wed 24 apr 00h09

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var radiusX;
 var radiusY;
 var style;
-
 var words = ['epilepsy', 'warning'];
 var index = 0;
 var textSize;
 
 function setup() {
-  var canvas = createCanvas(1250, 600);
+  var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('myContainer');
   texto = createSlider(1, 300, 1, 0.01);
   texto.parent('myInputs');
@@ -41,21 +40,37 @@ function draw() {
   }
   endShape();
 
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += sty) {
+    let r = random(rX, rY);
+  	let x = r * cos(a);
+  	let y = r * sin(a);
+    vertex(x, y);
+  }
+  endShape();
+
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += sty) {
+    let r = random(rX, rY);
+  	let x = r * cos(a);
+  	let y = r * sin(a);
+    vertex(x, y);
+  }
+  endShape();
+
+if(mouseIsPressed) {
   textAlign(CENTER);
   fill(random(255), random(255), random(255));
   noStroke();
   textSize(textS);
   text(words[index], 0, 0);
   index++;
+}
   
   if (index > 10) {
     index = 0;
   }
 
-}
-
-function mousePressed (){
-  windowResized();
 }
 
 function windowResized(){
